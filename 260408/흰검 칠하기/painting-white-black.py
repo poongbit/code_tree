b3af45357e8bef48d270e46f4324bed07c48bf61
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-08 22:54:56 +0900

    Title: 흰검 칠하기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260408/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,60 @@
+n = int(input())
+commands = [tuple(input().split()) for _ in range(n)]
+x = []
+dir = []
+for num, direction in commands:
+    x.append(int(num))
+    dir.append(direction)
+
+# Please write your code here.
+
+OFFSET = 500
+MAX_R = 1000
+
+line = [0] * 2 * 1000
+cnt_b = [0] * 2000
+cnt_w = [0] * 2000
+
+cur = 500
+
+for i in range(n):
+    dist, direction = x[i],dir[i]
+
+    if direction == 'L':
+        # 왼쪽으로 칠함
+        while dist > 0:
+            line[cur] = 1
+            cnt_w[cur] +=1
+
+            dist -= 1
+
+            if dist:
+                cur -= 1
+
+    else:
+        # 오른쪽으로 칠함
+        line[cur] = 2
+        cnt_b[cur]  +=1
+        dist -= 1
+
+        if dist:
+            cur +=1 
+
+g,w,b = 0,0,0
+
+for i in range(len(line)):
+
+    # 검은색과 흰 색 두번 이상 칠해져 있으면 회색
+    if cnt_b[i] >= 2 and cnt_w[i] >=2:
+        g +=1
+
+    # 그렇지 않으면 현재 칠해진 색깔이 곧 타일의 색깔
+
+    elif line[i] == 1:
+        w +=1
+
+    elif line[i] == 2:
+        b +=1
+
+print(w,b,g)
+
