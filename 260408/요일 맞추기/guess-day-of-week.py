50848216e421962146ebcc40b415a1b416e3db2b
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-08 15:41:31 +0900

    Title: 요일 맞추기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260408/요일 맞추기/guess-day-of-week.py
+++ 260408/요일 맞추기/guess-day-of-week.py
@@ -1,7 +1,7 @@
 m1,d1,m2,d2 = map(int, input().split())
 
 # Please write your code here.
-month = [31,28,31,30,31,30,31,31,30,31,30,31]
+month = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 
 first_time = month[m1] + d1
 second_time = month[m2] + d2
@@ -9,8 +9,7 @@ second_time = month[m2] + d2
 result = abs(first_time - second_time)
 
 
-date = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+date = [0,'Sun','Mon','Tue','Wed','Thu','Fri','Sat']
 
-index = (result % 7) -1
 
-print(date[index])
+print(date[result % 7])
