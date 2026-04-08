5dff59ea07dd6719c6d8ef1d93e6daa161e88eb6
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-08 17:08:32 +0900

    Title: 그 요일은, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260408/그 요일은/the-day-of-the-day.py
+++ 260408/그 요일은/the-day-of-the-day.py
@@ -3,7 +3,7 @@ A = input()
 
 # Please write your code here.
 
-month = [31,28,31,30,31,30,31,31,30,31,30,31]
+month = [0,31,29,31,30,31,30,31,31,30,31,30,31]
 
 # A의 인덱스 위치 파악하기
 day_of_week = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
@@ -15,8 +15,6 @@ for i in range(len(day_of_week)):
         A_index = i
         break
 
-
-
 count = 0
 
 # 시작 요일은 월요일
@@ -24,15 +22,19 @@ start_index = 0
 
 while True:
     if m1 == m2 and d1 == d2:
+        start_index %= 7
+        if start_index == A_index:
+            count +=1
+        
         break
-    d1 +=1
-
     # 인덱스 증가 가능 여부 체크 
 
-    start_index %=7
+    start_index %= 7
 
     if start_index == A_index:
         count +=1
+
+    d1 +=1
     
     start_index += 1
 
