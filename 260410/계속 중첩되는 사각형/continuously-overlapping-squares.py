ef883f7eb7664efcd62e42d812d1c526ebd7b849
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-10 02:01:20 +0900

    Title: 계속 중첩되는 사각형, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260410/계속 중첩되는 사각형/continuously-overlapping-squares.py
@@ -0,0 +1,38 @@
+n = int(input())
+x1, y1, x2, y2 = [], [], [], []
+for _ in range(n):
+    a, b, c, d = map(int, input().split())
+    x1.append(a)
+    y1.append(b)
+    x2.append(c)
+    y2.append(d)
+
+# Please write your code here.
+
+offset = 100
+MAX_K = 200
+
+area = [[0] * (MAX_K+1) for _ in range(MAX_K+1)]
+
+max_color = 0
+
+for i in range(n):
+    a,b,c,d = x1[i] + offset, y1[i] + offset, x2[i] + offset, y2[i] + offset
+    
+    max_color = max(max_color,i)
+
+    for row in range(a,c):
+        for column in range(b,d):
+            area[row][column] = i
+
+count = 0 
+
+if max_color % 2 !=0:
+    # 파란색임
+    for row in range(len(area)):
+        for column in range(len(area[0])):
+            if area[row][column] == max_color:
+                count +=1
+
+
+print(count)
