1b422b06f3ac51232ee494e0e2461d9a17a4600b
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-10 02:28:21 +0900

    Title: 연속되는 수 2, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260410/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,31 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+
+cnt = 0
+
+
+def check_num(arr):
+    n = len(arr)
+    cnt_arr = []
+    cnt = 0
+
+    for i in range(n):
+        if i == 0:
+            cnt += 1
+    
+        elif arr[i] != arr[i-1]:
+            cnt_arr.append(cnt)
+            cnt = 0
+            cnt +=1
+        
+        else:
+            cnt +=1
+
+    
+    return cnt_arr
+
+result = check_num(arr)
+
+print(max(result))
