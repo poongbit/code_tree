e4238db1ca9804f50cb6a701dc3e8b75196401d1
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-10 23:41:30 +0900

    Title: 연속되는 수 4, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260410/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,18 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+cnt = 0
+ans = 0
+
+for i in range(n):
+    if i>0 and arr[i]> arr[i-1]:
+        cnt +=1
+
+    else:
+        cnt = 1
+
+
+    ans = max(ans,cnt)
+
+print(ans)
