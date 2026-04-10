0d51085373b394d969adb3517e185b1523926640
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-10 16:20:18 +0900

    Title: 연속되는 수 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260410/연속되는 수 2/continuous-number2.py
+++ 260410/연속되는 수 2/continuous-number2.py
@@ -3,32 +3,19 @@ arr = [int(input()) for _ in range(n)]
 
 # Please write your code here.
 
-cnt = 0
+cnt = 1
+connected = []
 
+for i in range(n):
+    if i == 0 or arr[i] != arr[i-1]:
+        connected.append(cnt)
+        cnt = 1
 
-def check_num(arr):
-    n = len(arr)
-    cnt_arr = []
-    cnt = 0
+    else:
+        cnt +=1
 
-    if n == 1:
-        cnt_arr.append(1)
-
-    for i in range(n):
-        if i == 0:
-            cnt += 1
-    
-        elif arr[i] != arr[i-1]:
-            cnt_arr.append(cnt)
-            cnt = 0
-            cnt +=1
-        
-        else:
-            cnt +=1
+print(max(connected))
 
     
-    return cnt_arr
+    
 
-result = check_num(arr)
-
-print(max(result))
