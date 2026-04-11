ebcd46d5440a26d5ceea3c077ddd973ef7f45478
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-11 15:32:32 +0900

    Title: 벌금은 누구에게, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260411/벌금은 누구에게/who-will-pay.py
+++ 260411/벌금은 누구에게/who-will-pay.py
@@ -5,10 +5,17 @@ student = [int(input()) for _ in range(M)]
 
 punishment = [0] * (len(student) +1)
 
+need_punish = False
+
 for member in student:
     # 벌점 기록
 
     punishment[member] +=1
 
     if punishment[member] == K:
+        need_punish = True
         print(member)
+
+
+if not need_punish:
+    print(-1)
