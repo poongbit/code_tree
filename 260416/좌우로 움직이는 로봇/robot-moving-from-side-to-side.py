938557d520ede4673e17027e7328e27289b88cad
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-16 15:56:31 +0900

    Title: 좌우로 움직이는 로봇, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260416/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -0,0 +1,66 @@
+n, m = map(int, input().split())
+
+# Process robot A's movements
+t = []
+d = []
+for _ in range(n):
+    time, direction = input().split()
+    t.append(int(time))
+    d.append(direction)
+
+# Process robot B's movements
+t_b = []
+d_b = []
+for _ in range(m):
+    time, direction = input().split()
+    t_b.append(int(time))
+    d_b.append(direction)
+
+# Please write your code here.
+
+MAX_T = 1000000
+
+a_time = [0] * (MAX_T +1)
+b_time = [0] * (MAX_T +1)
+
+# A의 움직임 기록
+time_a =1
+
+for i in range(n):
+    time,direction = t[i],d[i]
+
+    if direction == 'R':
+        for _ in range(time):
+            a_time[time_a] = a_time[time_a-1] + 1
+            time_a +=1
+
+    else:
+        for _ in range(time):
+            a_time[time_a] = a_time[time_a-1] -1
+            time_a +=1
+
+time_b = 1
+
+for i in range(m):
+    time, direction = t_b[i], d_b[i]
+
+    if direction == 'R':
+        for _ in range(time):
+            b_time[time_b] = b_time[time_b-1] +1
+            time_b +=1
+
+        
+    else:
+        for _ in range(time):
+            b_time[time_b] = b_time[time_b-1] -1
+            time_b +=1
+
+count = 0
+
+max_time = max(time_a,time_b)
+
+for i in range(1,max_time+1):
+    if a_time[i-1] != b_time[i-1] and a_time[i] == b_time[i]:
+        count +=1
+
+print(count)
