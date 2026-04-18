374f217a3c4f8551963d2372e8e31ab766a3d022
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-18 22:23:41 +0900

    Title: 방향에 맞춰 이동, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260418/방향에 맞춰 이동/move-in-direction.py
@@ -0,0 +1,32 @@
+n = int(input())
+moves = [tuple(input().split()) for _ in range(n)]
+dir = [move[0] for move in moves]
+dist = [int(move[1]) for move in moves]
+
+# Please write your code here.
+x,y = 0,0
+
+for i in range(n):
+    direction, distance = dir[i],dist[i]
+
+    # 동,서,남,북
+    dx = [1,-1,0,0]
+    dy = [0,0,1,1]
+
+    if direction == 'E':
+        x += dx[0] * distance
+        y += dy[0] * distance
+    
+    elif direction == 'W':
+        x += dx[1] * distance
+        y += dy[1] * distance
+
+    elif direction == 'S':
+        x += dx[2] * distance
+        y += dy[2] * distance
+
+    elif direction == 'N':
+        x += dx[3] * distance
+        y += dy[3] * distance
+
+print(x,y)
