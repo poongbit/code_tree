5b80a1f3b58dfa214008ff309f98dd5bf20eacdc
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-19 21:49:59 +0900

    Title: 작은 구슬의 이동, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260419/작은 구슬의 이동/small-marble-movement.py
+++ 260419/작은 구슬의 이동/small-marble-movement.py
@@ -4,35 +4,40 @@ r, c = int(r), int(c)
 
 # Please write your code here.
 
+# 0-based index에 어룰리게 변경
+row,column = r-1,c-1
 
-mapper = {
+# (0번 3번)과 (1번 2번)이 서로 반대가 되도록 설계
+
+# 상,좌,우,하
+
+dx = [0,-1,1,0]
+dy = [-1,0,0,1]
+
+mapping = {
     'U' : 0,
     'L' : 1,
     'R' : 2,
     'D' : 3
 }
 
-row,column,move_dir = r-1,c-1,mapper[dir]
+# 초기 방향 채택
+move_dir = mapping[dir]
 
-# 상,좌,우,하
-dx = [0,-1,1,0]
-dy = [-1,0,0,1]
-
-# 범위 내에 들어갔는 지 확인
-
+# 좌표 안에 들어갔는 지 확인
 def in_range(row,column):
-    return 0<= row < n and 0<=column<n
-
+    return 0<=row<n and 0<=column<n
 
 # 시뮬레이션 진행
 for _ in range(t):
     n_row,n_column = row + dy[move_dir], column + dx[move_dir]
 
-    if not in_range(n_row,n_column):
-        move_dir = 3- move_dir
-
-    else:
+    if in_range(n_row,n_column):
+        # 그대로 바꿔줌
         row,column = n_row,n_column
 
+    else:
+        # 방향을 90도로 바꿈
+        move_dir = 3 - move_dir
 
 print(row+1,column+1)
