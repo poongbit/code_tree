e46cb3de1fc29daf9c00fa84ecfdfa22e8d0323b
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-21 22:56:10 +0900

    Title: Carry 피하기 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260421/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,42 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+
+
+# 자릿수 반환 함수
+def digit_return(num,pos):
+    return (num // 10 *pos) % 10
+
+
+result = 0
+
+# 서로 다른 3개의 수를 고름
+
+for i in range(n):
+    for j in range(i+1,n):
+        for k in range(j+1,n):
+            
+            # 숫자 3개를 지정함
+            num1,num2,num3 = arr[i], arr[j], arr[k]
+
+            max_len = max(len(str(num1)), len(str(num2)), len(str(num3)))
+
+            not_carry = True
+
+            for pos in range(max_len):
+                if digit_return(num1,pos) + digit_return(num2,pos) + digit_return(num3,pos) >= 10:
+                    not_carry = False
+                    break
+
+            if not_carry:
+                num_sum = num1 + num2 + num3
+
+                result = max(result,num_sum)
+
+
+print(result)                 
+
+
+
+                
