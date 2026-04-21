684ded6cb0d9431bb29ab520aece184484b0ef55
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-21 15:15:13 +0900

    Title: 이상한 진수 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260421/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,25 @@
+import sys
+INT_MIN = -sys.maxsize
+
+a = list(map(int,input()))
+# Please write your code here.
+
+# 2-digits로 변환하기
+
+result = INT_MIN
+
+for i in range(1,len(a)):
+    origin = a[i]
+
+    # 0 ->1 / 1->0 변환
+    a[i] = (a[i]+1) % 2
+
+    num = 0
+    for j in range(len(a)):
+        num = num * 2 + a[j]
+
+    result = max(result,num)
+
+    a[i] = origin
+
+print(result)
