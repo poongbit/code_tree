eb846a10484c929e1f0264168c43743682453745
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-21 23:18:14 +0900

    Title: Carry 피하기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260421/Carry 피하기 2/escaping-carry-2.py
+++ 260421/Carry 피하기 2/escaping-carry-2.py
@@ -6,50 +6,50 @@ arr = [int(input()) for _ in range(n)]
 
 # Please write your code here.
 
-
-# 자릿수 반환 함수
-def digit_return(num,pos):
+def digit_number(num,pos):
     return (num // 10 ** pos) % 10
 
-
+# 가장 큰 값 저장할 변수 생성 
 result = INT_MIN
 
-# 하나의 조합이라도 있으면 found는 True로 바뀔 예정
+# 실제로 carry가 발생하는 조합이 있는 지 체크
 found = False
 
-
-# 서로 다른 3개의 수를 고름
+# 숫자 3개 고르기
 
 for i in range(n):
     for j in range(i+1,n):
         for k in range(j+1,n):
-            
-            # 숫자 3개를 지정함
-            num1,num2,num3 = arr[i], arr[j], arr[k]
+
+            # 숫자 3개 선택 
+            num1, num2, num3 = arr[i], arr[j], arr[k]
+
+            # 각 자릿수 뽑기
+
+            # 숫자들 중 가장 긴 숫자 찾기
 
             max_len = max(len(str(num1)), len(str(num2)), len(str(num3)))
 
-            not_carry = True
+            is_carry = False
 
-            for pos in range(max_len):
-                if digit_return(num1,pos) + digit_return(num2,pos) + digit_return(num3,pos) >= 10:
-                    not_carry = False
+            for p in range(max_len):
+                if digit_number(num1,p) + digit_number(num2,p) + digit_number(num3,p) >= 10:
+                    is_carry = True
                     break
 
-            if not_carry:
+            if is_carry:
+                continue
+
+            else:
                 found = True
                 num_sum = num1 + num2 + num3
-
                 result = max(result,num_sum)
-
 
 
 if found:
     print(result)
 
 else:
-    print(-1)                 
-
-
+    print(-1)
 
                 
