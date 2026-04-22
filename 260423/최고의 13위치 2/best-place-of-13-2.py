d2cdb3e6e28f303c74e2a129eed1765aa5f4095e
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-23 00:33:41 +0900

    Title: 최고의 13위치 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260423/최고의 13위치 2/best-place-of-13-2.py
+++ 260423/최고의 13위치 2/best-place-of-13-2.py
@@ -5,14 +5,22 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 
 max_cnt = 0
 
-for row in range(n-1):
+for row in range(n):
     for col in range(n-2):
+        for n_row in range(n):
 
-        for n_row in range(row+1,n):
-            for n_col in range(n-2):
+            # 두 격자가 같은 행에 있는 경우
+            if row == n_row:
+                for n_col in range(col+3,n-2):
 
-                max_cnt = max(max_cnt,arr[row][col]+arr[row][col+1]+arr[row][col+2]
-                + arr[n_row][n_col] + arr[n_row][n_col+1] + arr[n_row][n_col+2])
+                    max_cnt = max(max_cnt,arr[row][col]+arr[row][col+1]+arr[row][col+2]
+                    + arr[n_row][n_col] + arr[n_row][n_col+1] + arr[n_row][n_col+2])
+
+            else:
+                for n_col in range(n-2):
+                    max_cnt = max(max_cnt,arr[row][col]+arr[row][col+1]+arr[row][col+2]
+                    + arr[n_row][n_col] + arr[n_row][n_col+1] + arr[n_row][n_col+2])
+
 
     
 print(max_cnt)
