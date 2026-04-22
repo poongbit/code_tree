d4c19f0c8dfc6d8a300264267e67f4e428ee2628
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-23 00:43:38 +0900

    Title: 최고의 13위치 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260423/최고의 13위치 2/best-place-of-13-2.py
+++ 260423/최고의 13위치 2/best-place-of-13-2.py
@@ -3,26 +3,27 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 
-max_cnt = 0
+max_count = 0
 
 for row in range(n):
-    for col in range(n-2):
+    for column in range(n-2):
+
         for n_row in range(n):
-
-            # 두 격자가 같은 행에 있는 경우
+            
+            # 같은 행에 속하는 경우
             if row == n_row:
-                for n_col in range(col+3,n-2):
+                for n_col in range(column+3,n-2):
 
-                    max_cnt = max(max_cnt,arr[row][col]+arr[row][col+1]+arr[row][col+2]
+                    max_count = max(max_count, arr[row][column] + arr[row][column+1] + arr[row][column+2]
                     + arr[n_row][n_col] + arr[n_row][n_col+1] + arr[n_row][n_col+2])
 
             else:
+                # 전 열 탐색 가능
                 for n_col in range(n-2):
-                    max_cnt = max(max_cnt,arr[row][col]+arr[row][col+1]+arr[row][col+2]
+                    max_count = max(max_count, arr[row][column] + arr[row][column+1] + arr[row][column+2]
                     + arr[n_row][n_col] + arr[n_row][n_col+1] + arr[n_row][n_col+2])
 
 
-    
-print(max_cnt)
-                
+print(max_count)
+                    
         
