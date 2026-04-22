8d535af74963e3d436f8847773517cab47b8e368
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-23 00:12:39 +0900

    Title: 최고의 13위치 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260423/최고의 13위치 2/best-place-of-13-2.py
@@ -0,0 +1,20 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+
+max_cnt = 0
+
+for row in range(n):
+    for col in range(n-2):
+
+        for n_row in range(row+1,n):
+            for n_col in range(n-2):
+
+                max_cnt = max(max_cnt,arr[row][col]+arr[row][col+1]+arr[row][col+2]
+                + arr[n_row][n_col] + arr[n_row][n_col+1] + arr[n_row][n_col+2])
+
+    
+print(max_cnt)
+                
+        
