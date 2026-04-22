3e067ff4f02ec3cd622da53396babe27ef1eebbd
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-23 01:35:40 +0900

    Title: 최고의 13위치 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260423/최고의 13위치 2/best-place-of-13-2.py
+++ 260423/최고의 13위치 2/best-place-of-13-2.py
@@ -3,27 +3,31 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 
-max_count = 0
+# 격자를 놓는다.
+max_cnt = 0
 
 for row in range(n):
+    # 격자를 벗어나지 않을 범위로만 잡는다.
     for column in range(n-2):
+        # 두 번째 격자를 놓는다. (n_row,n_col)
 
         for n_row in range(n):
-            
-            # 같은 행에 속하는 경우
-            if row == n_row:
-                for n_col in range(column+3,n-2):
+            # 격자를 벗어나지 않을 범위로만 잡는다.
+            for n_col in range(n-2):
 
-                    max_count = max(max_count, arr[row][column] + arr[row][column+1] + arr[row][column+2]
-                    + arr[n_row][n_col] + arr[n_row][n_col+1] + arr[n_row][n_col+2])
+                # 두 격자가 겹치는 경우에는 세지 않는다.
+                if row == n_row and abs(column-n_col) <= 2:
+                    continue
 
-            else:
-                # 전 열 탐색 가능
-                for n_col in range(n-2):
-                    max_count = max(max_count, arr[row][column] + arr[row][column+1] + arr[row][column+2]
-                    + arr[n_row][n_col] + arr[n_row][n_col+1] + arr[n_row][n_col+2])
+                
+
+                # 두 격자가 겹치지 않는 경우에 대해 동전 수를 세어 갱신해준다.
+                cnt1 = arr[row][column] + arr[row][column+1] + arr[row][column+2]
+                cnt2 = arr[n_row][n_col] + arr[n_row][n_col+1] + arr[n_row][n_col+2]
+
+                max_cnt = max(max_cnt,cnt1+cnt2)
 
 
-print(max_count)
-                    
+print(max_cnt)
+
         
