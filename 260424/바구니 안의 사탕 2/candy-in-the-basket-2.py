b3d54b7d80ae881c32c6039dce7441c9f9afb49b
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-24 22:19:29 +0900

    Title: 바구니 안의 사탕 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260424/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 260424/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -9,9 +9,9 @@ for _ in range(N):
 
 # Please write your code here.
 
-MAX_K = 1000
+MAX_K = 500
 
-locate = [0] * (MAX_K + 1)
+locate = [0] * (MAX_K + 10)
 
 # 사탕의 개수와 위치 기록하기 
 for i in range(N):
@@ -20,10 +20,9 @@ for i in range(N):
 result = 0
 
 
-
-for i in range(0,MAX_K - K + 1):
+for i in range(1,MAX_K - K + 2):
     max_candy = 0
-    for c in range(i-K,i+K+1):
+    for c in range(i-K, i+K+1):
         max_candy += locate[c]
 
     result = max(result,max_candy)
