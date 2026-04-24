96d7db1414985bea29a4ccaa9917650c24216f47
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-24 22:15:00 +0900

    Title: 바구니 안의 사탕 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260424/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,31 @@
+N, K = map(int, input().split())
+candy = []
+pos = []
+
+for _ in range(N):
+    c, p = map(int, input().split())
+    candy.append(c)
+    pos.append(p)
+
+# Please write your code here.
+
+MAX_K = 1000
+
+locate = [0] * (MAX_K + 1)
+
+# 사탕의 개수와 위치 기록하기 
+for i in range(N):
+    locate[pos[i]] = candy[i]
+
+result = 0
+
+
+
+for i in range(0,MAX_K - K + 1):
+    max_candy = 0
+    for c in range(i-K,i+K+1):
+        max_candy += locate[c]
+
+    result = max(result,max_candy)
+
+print(result)
