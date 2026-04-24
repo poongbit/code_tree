92236bc4322f030b1491b7625d3c494735d622f6
Young Hwi Kim <rladydgnj@gmail.com>
2026-04-24 22:57:26 +0900

    Title: 바구니 안의 사탕 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260424/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 260424/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -11,7 +11,7 @@ for _ in range(N):
 
 MAX_K = 100
 
-locate = [0] * (MAX_K + 10)
+locate = [0] * (MAX_K + 1)
 
 # 사탕의 개수와 위치 기록하기 
 for i in range(N):
@@ -20,7 +20,7 @@ for i in range(N):
 result = 0
 
 
-for i in range(0,MAX_K - K + 1):
+for i in range(0,MAX_K):
     max_candy = 0
     for c in range(i-K,i+K+1):
         # 주어진 위치가 넘어가서는 안된다
